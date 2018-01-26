7f954c1eca4438b7278bd410e14b977f86b3e6ad
Alejandro Hernandez <alejandro@brainhub.pl>
2018-01-26 19:16:38 +0100

    CI: Small fix

--- release.config.js
+++ release.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   analyzeCommits: {
     preset: 'eslint',
-    releaseRules: './config/release-rules.js', // optional, only if you want to set up new/modified release rules inside another file
     parserOpts: { // optional, only you want to have emoji commit support
       headerPattern: /^(?::([\w-]*):)?\s*(\w*):\s*(.*)$/,
       headerCorrespondence: [
