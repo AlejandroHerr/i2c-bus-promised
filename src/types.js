6badf81b347eb6b8c96faabe5b5f6e4868012bc3
Alejandro Hernandez <alejandro@brainhub.pl>
2018-02-05 11:09:34 +0100

    Style: Extra new lines

--- src/types.js
+++ src/types.js
@@ -2,11 +2,15 @@
 
 /** Single bit */
 export type BitType = 0 | 1;
+
 /** Byte-length number (0x00 - 0xff) */
 export type ByteType = number;
-/** Byte-length number (0x00 - 0xff) */
+
+/** Byte-length number (0x0000 - 0xffff) */
 export type WordType = number;
+
 /** Byte-length number (0x00 - 0xff) */
 export type AddrType = number;
+
 /** Byte-length number (0x00 - 0xff) */
 export type CmdType = number;
