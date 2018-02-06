0635cae6b61b4f79872f16e2bfa2e21e5fbb1c1a
Alejandro Hernandez <alejandro@brainhub.pl>
2018-02-06 22:56:12 +0100

    Chore: Export everything and build only source files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 // @flow
 import Bus from './Bus';
 import Device from './Device';
+import BusError from './error/BusError';
+import * as BUS_ERROR_CODES from './error/codes';
+import createI2cBus from './mock/createI2cBus';
+import i2cBus from './mock/i2cBus';
 
 export {
   Bus,
   Device,
+  BusError,
+  BUS_ERROR_CODES,
+  createI2cBus,
+  i2cBus,
 };
